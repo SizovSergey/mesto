46cc8e600f79dd0e295bbbfdefb2baafbdd717e7
Sergey Sizov <vpnsizov@yandex.ru>
2023-01-19 17:36:07 +0300

    изменения в elements

--- scripts/script.js
+++ scripts/script.js
@@ -3,25 +3,27 @@ let profileJob = document.querySelector('.profile__user-info');
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
+let nameInput = document.querySelector("input[name='name']");
+let jobInput = document.querySelector("input[name='job']");
 let closeButton = document.querySelector('.popup__cancel-button');
 let saveButton = document.querySelector('.popup__save-button');
 
 
 editButton.addEventListener('click', function() {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 });
 
-closeButton.addEventListener('click', function() {
-    popup.classList.remove('popup__opened');
-});
+function ClosePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+closeButton.addEventListener('click', ClosePopup);
 
 popupForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove('popup__opened');
+    ClosePopup ();
 }); 
